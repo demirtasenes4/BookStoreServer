67b9f581577ae1cd04eec2bd0cf14dc675a239b1
Quadesh <77413354+demirtasenes4@users.noreply.github.com>
2023-10-09 22:42:53 +0300

    Addded Context for Database and Value Objects to control the pricing.

--- BookStoreServer.WebApi/Context/AppDbContext.cs
+++ BookStoreServer.WebApi/Context/AppDbContext.cs
@@ -1,5 +1,26 @@
-﻿namespace BookStoreServer.WebApi.Context;
+﻿using BookStoreServer.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace BookStoreServer.WebApi.Context;
+
+public sealed class AppDbContext : DbContext
 {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=TUGAY\\SQLEXPRESS;Initial Catalog=YMYP_BookStoreDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<BookCategory> BookCategories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>().OwnsOne(p => p.Price, price =>
+        {
+            price.Property(p => p.Value).HasColumnType("money");
+            price.Property(p => p.Currency).HasMaxLength(5);
+        });
+        modelBuilder.Entity<BookCategory>().HasKey(p => new { p.BookId, p.CategoryId });
+    }
 }
---
+++ BookStoreServer.WebApi/ValueObjects/Money.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+namespace BookStoreServer.WebApi.ValueObjects;
+
+public sealed class Money
+{
+    public Money(decimal value, string currency)
+    {
+        Value = value;
+        Currency = currency;
+    }
+
+    public decimal Value { get; private set; }
+    public string Currency { get; private set; }
+}
